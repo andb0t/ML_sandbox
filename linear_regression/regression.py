e1a782700ba81f074cb7c625c48e4d6350698509
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-01-21 23:25:06 +0100

    Add stratified random sampling

--- linear_regression/regression.py
+++ linear_regression/regression.py
@@ -7,6 +7,8 @@ import numpy as np
 import pandas as pd
 
 
+SAVE_PLOTS = False
+
 print('Download the data')
 DOWNLOAD_ROOT = 'https://github.com/ageron/handson-ml/raw/master/'
 HOUSING_PATH = 'datasets/housing'
@@ -41,14 +43,15 @@ print(housing.info())  # shows column data types and general data frame info
 print(housing['ocean_proximity'].value_counts())  # shows value abundance
 print(housing.describe())  # shows count, mean, std etc for every column
 
-print('Plot the data')
-plt.figure()
-housing.hist(bins=50, figsize=(20, 15))
-plt.savefig('housing.pdf')
+if SAVE_PLOTS:
+    print('Plot the data')
+    plt.figure()
+    housing.hist(bins=50, figsize=(20, 15))
+    plt.savefig('housing.pdf')
 
 print('Split into train and test data set')
 
-split_strategy = 'hash_ID_homemade'
+split_strategy = 'sklearn_naive'
 
 if split_strategy == 'naive':
     def split_train_test(data, test_ratio):
@@ -82,5 +85,26 @@ elif split_strategy == 'hash_ID' or split_strategy == 'hash_ID_homemade':
         housing_with_id = housing
         housing_with_id['id'] = housing['longitude'] * 1000 + housing['latitude']
         train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, 'id')
+
+elif split_strategy == 'stratified':
+    # scale income (not strictly necessary)
+    housing['income_cat'] = np.ceil(housing['median_income'] / 1.5)
+    # split < 5 in 4 bins and fill rest in > 5 bin
+    housing['income_cat'].where(housing['income_cat'] < 5, 5, inplace=True)
+
+    print('Abundance in original dataset')
+    print(housing['income_cat'].value_counts() / len(housing))
+
+    from sklearn.model_selection import StratifiedShuffleSplit
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(housing, housing['income_cat']):
+        train_set = housing.loc[train_index]
+        test_set = housing.loc[test_index]
+
+    print('Abundance in stratified test set')
+    print(test_set['income_cat'].value_counts() / len(test_set))
+
+    print('Abundance in stratified train set')
+    print(train_set['income_cat'].value_counts() / len(train_set))
 
 print(len(train_set), 'train +', len(test_set), 'test')
