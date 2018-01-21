303712e6585365e4454238be6415e2af0d8dbbdd
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-01-21 13:33:23 +0100

    Add error visualization

--- multiclass_classification/classification.py
+++ multiclass_classification/classification.py
@@ -65,3 +65,63 @@ forest_clf.fit(X_train, y_train)
 result_forest = forest_clf.predict([some_digit])
 print('Result random forest', result_forest)
 print('Probabilities', forest_clf.predict_proba([some_digit]))
+
+print('Evaluate the classifiers using cross-validation')
+from sklearn.model_selection import cross_val_score
+cvs = cross_val_score(sgd_clf, X_train, y_train, cv=3, scoring='accuracy')
+print('Cross-validation score', cvs)
+
+print('Scale input to increase accuracy')
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train.astype(np.float64))
+cvs_scaled = cross_val_score(sgd_clf, X_train_scaled, y_train, cv=3, scoring='accuracy')
+print('Cross-validation score scaled training data', cvs_scaled)
+
+print('Error analysis')
+from sklearn.model_selection import cross_val_predict
+y_train_pred = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
+from sklearn.metrics import confusion_matrix
+conf_mx = confusion_matrix(y_train, y_train_pred)
+print('Confusion matrix', conf_mx)
+
+print('Normalize confusion matrix')
+row_sums = conf_mx.sum(axis=1, keepdims=True)
+norm_conf_mx = conf_mx / row_sums
+np.fill_diagonal(norm_conf_mx, 0)
+print('Scaled confusion matrix', norm_conf_mx)
+
+plt.figure()
+plt.matshow(norm_conf_mx, cmap=plt.cm.gray)
+plt.savefig('confusion_matrix.pdf')
+
+print('Plot some of the often falsely classified images')
+
+
+def plot_digits(instances, images_per_row=10, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size, size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row: (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap=matplotlib.cm.binary, **options)
+    plt.axis("off")
+
+cl_a, cl_b = 3, 5
+X_aa = X_train[(y_train == cl_a) & (y_train_pred == cl_a)]
+X_ab = X_train[(y_train == cl_a) & (y_train_pred == cl_b)]
+X_ba = X_train[(y_train == cl_b) & (y_train_pred == cl_a)]
+X_bb = X_train[(y_train == cl_b) & (y_train_pred == cl_b)]
+
+plt.figure(figsize=(8, 8))
+plt.subplot(221); plot_digits(X_aa[:25], images_per_row=5)
+plt.subplot(222); plot_digits(X_ab[:25], images_per_row=5)
+plt.subplot(223); plot_digits(X_ba[:25], images_per_row=5)
+plt.subplot(224); plot_digits(X_bb[:25], images_per_row=5)
+plt.savefig('errors_visualized.pdf')
