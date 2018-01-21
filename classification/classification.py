8a01b56f7719997edb7e9333c6ea7064b310e24a
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-01-21 12:04:08 +0100

    Add precision and recall plots

--- classification/classification.py
+++ classification/classification.py
@@ -13,10 +13,15 @@ print(y.shape)
 
 # Inspect one instance
 some_digit = X[36000]
+
+
+def show_image(some_digit_image):
+    plt.figure()
+    plt.imshow(some_digit_image, cmap=matplotlib.cm.binary, interpolation='nearest')
+    plt.savefig('example_instance.pdf')
+
 some_digit_image = some_digit.reshape(28, 28)
-plt.imshow(some_digit_image, cmap=matplotlib.cm.binary, interpolation='nearest')
-plt.axis('off')
-plt.savefig('test.pdf')
+show_image(some_digit_image)
 
 # Get the test data
 MNIST_TRAIN_DIV = 60000
@@ -33,8 +38,54 @@ y_train_5 = (y_train == 5)
 y_test_5 = (y_test == 5)
 
 from sklearn.linear_model import SGDClassifier
-
-sgd_clf = SGDClassifier(random_state=1337)
+#
+sgd_clf = SGDClassifier(random_state=42)
 sgd_clf.fit(X_train, y_train_5)
 result = sgd_clf.predict([some_digit])
 print(result)
+
+print('Cross validation:')
+
+from sklearn.model_selection import cross_val_score
+cvs = cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring='accuracy')
+print(cvs)
+
+print('Confusion matrix:')
+
+from sklearn.model_selection import cross_val_predict
+
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+print('confusion_matrix:', confusion_matrix(y_train_5, y_train_pred))
+print('precision_score:', precision_score(y_train_5, y_train_pred))
+print('recall_score:', recall_score(y_train_5, y_train_pred))
+print('f1_score:', f1_score(y_train_5, y_train_pred))
+
+print('Get decision scores:')
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method='decision_function')
+from sklearn.metrics import precision_recall_curve
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+
+
+def plot_precision_recall_vs_threshold(prec, rec, thresh):
+    plt.figure()
+    plt.plot(thresh, prec[:-1], 'b--', label='Precision')
+    plt.plot(thresh, rec[:-1], 'g--', label='Recall')
+    plt.xlabel('Threshold')
+    plt.legend(loc='upper left')
+    plt.ylim([0, 1])
+
+
+def plot_precision_vs_recall(prec, rec):
+    plt.figure()
+    plt.plot(rec, prec, 'b--', label='Precision')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0, 1])
+
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+plt.savefig('precision_recall_threshold_curve.pdf')
+
+plot_precision_vs_recall(precisions, recalls)
+plt.savefig('precision_recall_curve.pdf')
