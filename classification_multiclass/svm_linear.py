4b7c13119e8fe2a9b18fea9c036ec2b29c00a6f0
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-01-23 00:20:05 +0100

    Add SVM via SGD

--- classification_multiclass/svm_linear.py
+++ classification_multiclass/svm_linear.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn import datasets
+from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
@@ -16,7 +17,7 @@ print(iris.data[0, (2, 3)])
 X = iris['data'][:, (2, 3)]  # petal length and width
 y = (iris['target'] == 2).astype(np.float64)  # Iris-Virginica
 
-SVM_algo = 'linear_optimized'
+SVM_algo = 'SGD_SVC'
 
 if SVM_algo == 'linear_optimized':
     svm_clf = Pipeline((
@@ -28,6 +29,11 @@ elif SVM_algo == 'linear':
     svm_clf = Pipeline((
         ('scaler', StandardScaler()),
         ('linear_svc', SVC(kernel='linear', C=10)),
+        ))
+elif SVM_algo == 'SGD_SVC':
+    svm_clf = Pipeline((
+        ('scaler', StandardScaler()),
+        ('linear_svc', SGDClassifier(loss='hinge')),  # hinge loss results in SVM
         ))
 
 svm_clf.fit(X, y)
