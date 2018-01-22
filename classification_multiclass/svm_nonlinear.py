871ca6f31e7ac5ffa12867b5537b95137f9418d1
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-01-23 00:09:19 +0100

    Add different kernels

--- classification_multiclass/svm_nonlinear.py
+++ classification_multiclass/svm_nonlinear.py
@@ -4,6 +4,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 
 print('Generate toy data')
 X, y = make_moons(n_samples=100, noise=0.1, random_state=42)
@@ -17,11 +18,25 @@ plt.scatter(list(map(lambda x: x[0], X)), list(map(lambda x: x[1], X)), c=y)
 plt.savefig('moon_scatter.pdf')
 
 print('Define model')
-polynomial_svm_clf = Pipeline((
-    ('poly_features', PolynomialFeatures(degree=3)),
-    ('scaler', StandardScaler()),
-    ('svm_clf', LinearSVC(C=10, loss='hinge')),
-    ))
+
+SVM_kernel = 'gaussian_rbf'
+
+if SVM_kernel == 'linear':
+    polynomial_svm_clf = Pipeline((
+        ('poly_features', PolynomialFeatures(degree=3)),
+        ('scaler', StandardScaler()),
+        ('svm_clf', LinearSVC(C=10, loss='hinge')),
+        ))
+elif SVM_kernel == 'polynomial':
+    polynomial_svm_clf = Pipeline((
+        ('scaler', StandardScaler()),
+        ('svm_clf', SVC(kernel='poly', degree=3, coef0=1, C=10)),
+        ))
+elif SVM_kernel == 'gaussian_rbf':
+    polynomial_svm_clf = Pipeline((
+        ('scaler', StandardScaler()),
+        ('svm_clf', SVC(kernel='rbf', gamma=5, C=0.001)),
+        ))
 
 print('Train model')
 polynomial_svm_clf.fit(X, y)
