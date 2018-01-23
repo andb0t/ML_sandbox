3f74660573411c93832cb574ca8b7bc64d276830
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-01-23 12:02:21 +0100

    CRLF -> LF

--- regression_nonlinear/regression.py
+++ regression_nonlinear/regression.py
@@ -11,6 +11,7 @@ def poly_noise(x, noise=0):
     return a + b * x + c * x ** 2 + noise * np.random.randn()
 
 x = 2 * np.random.random(100) - 1
+X = list(map(lambda x: [x], x))
 y = list(map(lambda x: poly_noise(x, noise=0.5), x))
 
 print('Plot it')
@@ -23,7 +24,7 @@ plt.savefig('poly_scatter.pdf')
 
 print('Define the model')
 svm_poly_reg = SVR(kernel='poly', degree=2, C=100, epsilon=0.1)
-svm_poly_reg.fit(list(map(lambda x: [x], x)), y)
+svm_poly_reg.fit(X, y)
 
 print('Prediction', svm_poly_reg.predict([[0.5]]))
 print('Parameters', svm_poly_reg.get_params())
