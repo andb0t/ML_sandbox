9f2ef01bd48a207fb2ad9b55e931981274db8af2
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-01-27 16:00:18 +0100

    Add gradient boost regressor

--- regression_decisiontree/regression_decisiontree.py
+++ regression_decisiontree/regression_decisiontree.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import numpy as np
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.tree import export_graphviz
 
@@ -20,9 +21,17 @@ x = 2 * np.random.random(100) - 1
 X = list(map(lambda x: [x], x))
 y = list(map(lambda x: poly_noise(x, noise=0.5), x))
 
-print('Define and train decision tree regressor')
+print('Define and train decision tree regressors')
 tree_reg = DecisionTreeRegressor(max_depth=2, random_state=42)
-tree_reg.fit(X, y)
+grb_reg = GradientBoostingRegressor(max_depth=2, n_estimators=3, learning_rate=1.0)
+
+print('Visualize training')
+for reg in (tree_reg, grb_reg):
+    reg.fit(X, y)
+    name = reg.__class__.__name__
+    my_plots.plot_reg_train_scatter(X, y, reg,
+                                    title=name,
+                                    save_name='reg_model_' + name + '.png')
 
 print('Display decision tree')
 export_graphviz(
@@ -31,13 +40,10 @@ export_graphviz(
     # feature_names=['y'],
     rounded=True,
     filled=True,
-    )
+)
 os.system('dot -Tpng poly_tree.dot -o poly_tree.png')
 
 print('Use model')
 print('(predicted value = average of all training instances in leaf)')
 result = tree_reg.predict([[0.6]])
 print('Result', result)
-
-print('Visualize training')
-my_plots.plot_reg_train_scatter(X, y, tree_reg)
