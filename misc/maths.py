d2441349077f5fc4115f88ff5277e09d976deead
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-01-29 00:52:32 +0100

    Include more PCA variants

--- misc/maths.py
+++ misc/maths.py
@@ -2,9 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.datasets import make_swiss_roll
+from sklearn.decomposition import PCA
+from sklearn.decomposition import IncrementalPCA
+
 
 print('Load the 3d dataset')
-X, t = make_swiss_roll(n_samples=100, noise=0, random_state=1337)
+X, t = make_swiss_roll(n_samples=200, noise=0, random_state=1337)
 
 y = X[:, 0] < 5
 
@@ -18,7 +21,9 @@ ax.set_title('Swiss roll example')
 plt.savefig("3d_dataset.png")
 
 print('Do PCA')
-PCA_strategy = 'sklearn'
+
+PCA_strategy = 'randomized'
+
 if PCA_strategy == 'by_hand':
     print('Center the dataset')
     X_centered = X - X.mean(axis=0)
@@ -47,9 +52,56 @@ if PCA_strategy == 'by_hand':
     print(W2)
     print('X2D')
     print(X2D)
-elif PCA_strategy == 'sklearn':
-    from sklearn.decomposition import PCA
+elif PCA_strategy == '2comp':
     pca = PCA(n_components=2)
-    X2D = pca.fit_transform(X)
+    X2D = pca.fit(X)
+    c1 = pca.components_.T[:, 0]
+    c2 = pca.components_.T[:, 1]
+    print('c1')
+    print(c1)
+    print('c2')
+    print(c2)
+    X2D = pca.transform(X)
     print('X2D')
     print(X2D)
+    print('explained variance ratio:', pca.explained_variance_ratio_)
+
+    plt.figure()
+    plt.scatter(X2D[:, 0], X2D[:, 1])
+    plt.savefig('x_reduced.png')
+
+elif PCA_strategy == 'cumsum':
+    pca = PCA()
+    pca.fit(X)
+    cumsum = np.cumsum(pca.explained_variance_ratio_)
+    d = np.argmax(cumsum > 0.95) + 1
+
+    print('Optimal dimensionality', d)
+    plt.figure()
+    plt.scatter(list(range(1, len(cumsum) + 1)), cumsum)
+    plt.xlabel('Dimension')
+    plt.ylabel('Explained variance')
+    plt.savefig('expl_var_ratio.png')
+
+    X_reduced = pca.tansform(X)
+    print(X_reduced)
+
+elif PCA_strategy == '0p95_var':
+    pca = PCA(n_components=0.95)
+    X_reduced = pca.fit_transform(X)
+    print(X_reduced)
+
+elif PCA_strategy == 'incremental':
+    # alternatively use mp.memmap for loading file in batches
+
+    n_batches = 5
+    inc_pca = IncrementalPCA(n_components=2)
+    for X_batch in np.array_split(X, n_batches):
+        inc_pca.partial_fit(X_batch)
+    X2D = inc_pca.transform(X)
+    print(D2D)
+
+elif PCA_strategy == 'randomized':
+    rnd_pca = PCA(n_components=2, svd_solver='randomized')
+    X_reduced = rnd_pca.fit_transform(X)
+    print(X_reduced)
