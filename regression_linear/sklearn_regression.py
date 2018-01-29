4132c956da47eb5e126ce451e2a68b31903bbb07
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-01-29 12:17:08 +0100

    Add ridge, lasso, SGD, ElasticNet

--- regression_linear/sklearn_regression.py
+++ regression_linear/sklearn_regression.py
@@ -5,7 +5,11 @@ import urllib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import Lasso
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import SGDRegressor
 from sklearn.svm import LinearSVR
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../utils'))
@@ -59,19 +63,15 @@ country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
 X = np.c_[country_stats['GDP per capita']]
 y = np.c_[country_stats['Life satisfaction']]
 
+print('Scale X')
+X = X / 10000
+
 # Visualize the data
 country_stats.plot(kind='scatter', x='GDP per capita', y='Life satisfaction')
 plt.savefig('life_satisfaction.pdf')
 
 # Select a linear model
-regression_model = 'LinearRegression'
-if regression_model == 'LinearRegression':
-    lin_reg = LinearRegression()
-elif regression_model == 'SVM':
-    lin_reg = LinearSVR(epsilon=1.5)
-
-# Train the model
-print('Train the model')
+lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
 print(type(X))
@@ -84,5 +84,26 @@ X_new = [[22587]]  # Cyprus' GDP per capita
 result = lin_reg.predict(X_new)
 print('Prediction for Cyprus:', result)
 
-print('Visualize training')
-my_plots.plot_reg_train_scatter(X, y, lin_reg)
+svm_reg = LinearSVR(epsilon=0.5)  # needs feature scaling
+sgd_reg = SGDRegressor(max_iter=50, penalty=None, eta0=0.1)
+ridge_reg = Ridge(alpha=1, solver='cholesky')
+lasso_reg = Lasso(alpha=0.1)
+elnet_reg = ElasticNet(alpha=0.1, l1_ratio=0.5)
+
+print('Train all of them and check their accuracy')
+# headers = ['Algorithm', 'Accuracy']
+# table = []
+for reg in (lin_reg, svm_reg, sgd_reg, ridge_reg, lasso_reg, elnet_reg):
+    name = reg.__class__.__name__
+    print('Fitting', name)
+    reg.fit(X, y)
+    # y_pred = reg.predict(X)
+    # accuracy = round(accuracy_score(y_test, y_pred), 3)
+    # table.append([name, accuracy])
+    my_plots.plot_reg_train_scatter(
+        X, y, reg,
+        title=name,  # + ' (' + str(accuracy) + ')',
+        save_name='reg_model_' + name + '.png')
+
+# print(tabulate.tabulate(sorted(table, key=lambda tup: tup[1], reverse=True),
+#                         headers=headers, tablefmt='grid'))
