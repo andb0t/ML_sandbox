3fb3f987805aefaeaa907e77e584ede54778f771
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-02-03 06:39:08 +0100

    Limit GPU RAM grabbed by app

--- tensorflow_graphs/gpu_usage.py
+++ tensorflow_graphs/gpu_usage.py
@@ -8,9 +8,10 @@ with tf.device('/gpu:0'):
 
 c = a * b
 
-print('Log placement')
+print('Configure session')
 config = tf.ConfigProto()
 config.log_device_placement = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.5  # only use half the RAM on each GPU
 sess = tf.Session(config=config)
 
 print('Run session')
